eac1120fb4f8cd331132052f1d3275d39e58975d
MinnKhantThwin <minnkhanthwin@gmail.com>
2024-01-28 22:52:59 +0630

    finished User CRUD

--- src/main/java/com/hostmdy/cinema/domain/UserPayment.java
+++ src/main/java/com/hostmdy/cinema/domain/UserPayment.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -24,6 +25,7 @@ public class UserPayment {
 	private Integer expiryMonth;
 	private Integer expiryYear;
 	
-
+	@OneToOne(mappedBy = "userPayment")
+	private User user;
 
 }
